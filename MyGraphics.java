7ad517d4ae3c685ebe1f83244cc1286d35a09fee
HurnyakYaroslav <hurnyakyaroslav@gmail.com>
2019-01-03 17:35:46 +0200

    Added buttons {+-/*}.

--- MyGraphics.java
+++ MyGraphics.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class MyGraphics {
     private static JFrame jFrame = new JFrame();
@@ -9,12 +11,53 @@ class MyGraphics {
         jFrame.setBounds(800, 200, 300,150);
         jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
         jFrame.setVisible(true);
+        JPanel  buttonPanel = drawButtons();
+        jFrame.setLayout(null);
+        jFrame.add(buttonPanel);
+        jFrame.setVisible(true);
+        jFrame.repaint();
+
     }
     //jPanel.setBounds(800, 200, 100,150);
 
-    private void drawButtons(){
+    private static JPanel drawButtons(){
         JPanel jPanel = new JPanel();
-        jPanel.setBounds(800, 200, 100,150);
+        jPanel.setBounds(0, 80, 300,30);
+        jPanel.setBackground(Color.gray);
+        jPanel.setVisible(true);
+        JButton button1 = new JButton("+");
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        JButton button2 = new JButton("-");
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        JButton button3 = new JButton("*");
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        JButton button4 = new JButton("/");
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        jPanel.add(button1);
+        jPanel.add(button2);
+        jPanel.add(button3);
+        jPanel.add(button4);
+
+        return jPanel;
     }
 }
